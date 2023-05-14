de9c63a0dca1ad0d06d921bf8791676730ef0a08
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-14 10:22:32 -0700

    button works

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
+// defines a function component (Button) that accepts a single argument, (props)/ Which represents properties passed ot the component.
 export default function Button(props) {
-   return <></>;
+  // the initial css for our button
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
