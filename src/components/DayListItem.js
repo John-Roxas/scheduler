b546aa98d825f9321a36684b2f9d50fbacff24b9
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-15 20:19:00 -0700

    add conditional statements to address 0 and 1 spot remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,10 +3,24 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  let spotText = "";
+  if (props.spots === 0) {
+    spotText = "no spots remaining";
+  } else if (props.spots === 1) {
+    spotText = "1 spot remaining";
+  } else {
+    spotText = props.spots + " spots remaining";
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotText}</h3>
     </li>
   );
 }
