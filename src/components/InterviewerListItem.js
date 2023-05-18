c6a1df739a60164dfc1b5408482da73a0a5f974f
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-17 18:11:33 -0700

    added conditional classes for selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,20 @@
 import React from "react";
 import classNames from "classnames";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+  const interviewersClass = classNames("interviewers", {
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
+        src={props.avatar}
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 }
