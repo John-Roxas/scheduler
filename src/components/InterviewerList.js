fa7d7492f3347ce8c39ea39fce89b7070339d63d
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-19 12:58:20 -0700

    refactored the component to list items dynamically

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,10 +5,21 @@ import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
+  let interviewers = props.interviewers;
+  let listItems = [];
+  interviewers.map((interviewer) => {
+    listItems.push(
+      <InterviewerListItem
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{listItems}</ul>
     </section>
   );
 }
