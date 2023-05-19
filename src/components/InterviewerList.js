92c1de4b1e90c4c85a842b18073b615836714da7
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-19 13:44:39 -0700

    got the setter to work for interviewerlist

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,12 +6,17 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
   let interviewers = props.interviewers;
+  // console.log(interviewers);
   let listItems = [];
   interviewers.map((interviewer) => {
+    // console.log(props.interviewer);
     listItems.push(
       <InterviewerListItem
+        id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
       />
     );
   });
