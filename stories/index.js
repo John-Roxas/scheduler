437cc4cab87fd24d24793b9b2c87a6a178e15850
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-21 09:38:46 -0700

    initialize stories for appointment in storybook

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -153,3 +154,7 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module).addParameters({
+  backgrounds: [{ name: "white", value: "#fff", default: true }],
+});
