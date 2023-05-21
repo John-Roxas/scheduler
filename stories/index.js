2389ae1b3254f3da4f0dab67de1d9e8d63a85057
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-21 09:41:52 -0700

    add an (appoint with time) story passing a prop of time set to 12pm

--- stories/index.js
+++ stories/index.js
@@ -155,6 +155,9 @@ storiesOf("InterviewerList", module)
     />
   ));
 
-storiesOf("Appointment", module).addParameters({
-  backgrounds: [{ name: "white", value: "#fff", default: true }],
-});
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
