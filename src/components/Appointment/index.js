854b966054cf602cd2131970365943c206835a6c
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-22 10:55:24 -0700

    implemented the rest of index.js to correctly use the Show and Empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,5 +5,22 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  // console.log(props.interviewer);
+  let display;
+  if (props.interview) {
+    display = (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    );
+  } else {
+    display = <Empty />;
+  }
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {display}
+    </article>
+  );
 }
