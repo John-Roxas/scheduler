0992c99e035c8159871af2a7116ed5fd31fe389f
TheLoneliestTime <john.a.roxas@gmail.com>
2023-05-22 10:31:34 -0700

    implemented form in storybook

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -189,5 +190,21 @@ storiesOf("Appointment", module)
     <Error
       message={"Could not delete appointment"}
       onClose={action("onClose")}
+    />
+  ))
+  .add("Form -- Edit", () => (
+    <Form
+      student={"Lydia Miller-Jones"}
+      interviewer={interviewers[0]}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form -- Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ));
